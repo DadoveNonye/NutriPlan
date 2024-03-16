d214ce3ff23262176a72581b56bd9fcf0f8751da
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-03-16 23:32:12 +0100

    {feat}: Add User model to models.py

--- app/models.py
+++ app/models.py
@@ -2,6 +2,28 @@
 """This module contains the models for NutriPlan application."""
 
 from app import db
+from typing import Optional
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    """ This class represents the user model. """
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True)
+
+    def set_password(self, password):
+        """ sets user password """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """ checks user password """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 class MealPlan(db.Model):
     """ This class represents the meal plan model. """
