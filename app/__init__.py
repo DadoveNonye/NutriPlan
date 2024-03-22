cdf0361ec6bfafc5f84ad8459c85296181ff0983
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-03-22 22:45:54 +0100

    {feat}: Refactor app initialization and blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,21 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    # 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
 from app import models
