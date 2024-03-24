81548c94e34e7c0ff26b3fed44ca39170cb7ac01
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-03-24 21:57:32 +0100

    {chore}: made comments

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,12 +6,18 @@ from app import db
 import sqlalchemy as sa
 
 class LoginForm(FlaskForm):
+    """
+    Form for user login.
+    """
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
+    """
+    Form for user registration.
+    """
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -20,22 +26,34 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        """
+        Validate if the username is already taken.
+        """
         user = db.session.scalar(sa.select(User).where(
             User.username == username.data))
         if user is not None:
             raise ValidationError('Please use a different username.')
         
     def validate_email(self, email):
+        """
+        Validate if the email address is already registered.
+        """
         user = db.session.scalar(sa.select(User).where(
             User.email == email.data))
         if user is not None:
             raise ValidationError('Please use a different email address.')
         
 class ResetPasswordRequestForm(FlaskForm):
+    """
+    Form for requesting password reset.
+    """
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
 
 class ResetPasswordForm(FlaskForm):
+    """
+    Form for resetting password.
+    """
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
