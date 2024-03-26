6ffe10c4af79c9a98fa45387cfb7da230f552d72
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-03-26 16:58:59 +0100

    {feat}: Add Flask-Login and Flask-Mail functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,33 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
+login = LoginManager()
+login.login_view = 'auth.login'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
+    login.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
-    # 
+    
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp)
+
 
     
 
