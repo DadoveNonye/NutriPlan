8b24d9bfb8b5a0f0db15c33604ff8d82a5cb5ef9
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-03-26 17:02:02 +0100

    {feat}: Add login requirement and GET method to /mealplans route

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,9 +3,14 @@ from app import db
 from app.models import MealPlan
 import sqlalchemy.orm as so
 from app.api import bp
+from flask_login import login_required, current_user
+from app.models import MealPlan
 
 
-@bp.route('/mealplans', methods=['POST'])
+
+
+@bp.route('/mealplans', methods=['POST', 'GET'])
+@login_required
 def create_meal_plan():
     data = request.get_json()
     name = data.get('name')
@@ -19,3 +24,15 @@ def create_meal_plan():
         'message': 'Meal plan created successfully',
         'meal_plan_id': meal_plan.id
     }), 201
+
+def get_meal_plans():
+        
+    # Get the logged-in user's ID from users.py
+    user_id = current_user.id
+
+    meal_plans = MealPlan.query.filter_by(user_id=user_id).all()
+
+    # Return a list of serialized MealPlan objects
+    meal_plans_data = [meal_plan.serialize() for meal_plan in meal_plans]
+
+    return jsonify(meal_plans_data), 200
