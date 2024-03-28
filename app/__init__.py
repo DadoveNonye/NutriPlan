3c0603a5b33e3f948f58a864c9bbf2e295baf88e
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-03-28 22:28:39 +0100

    {chore}: Remove errors blueprint and import statements

--- app/__init__.py
+++ app/__init__.py
@@ -19,10 +19,6 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     mail.init_app(app)
     login_manager.init_app(app)
-
-    from app.errors import bp as errors_bp
-    app.register_blueprint(errors_bp)
-
     
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
